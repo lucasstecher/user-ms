009f3ec9aed5e2dc9020821810bab8f35be2d7c3
Lucas Stecher <lucasvieira0070@gmail.com>
2025-09-27 15:01:11 -0300

    feat: create user repository

---
+++ src/main/java/dev/stecher/UserService/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package dev.stecher.UserService.repository;
+
+import dev.stecher.UserService.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<UserEntity, UUID> {
+}
